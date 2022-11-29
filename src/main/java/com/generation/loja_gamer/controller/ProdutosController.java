1614e403e208edca7867de36ff68259ae9d3a515
AdrianaMeli <adriana.nogueira@mercadolivre.com>
2022-11-29 14:03:52 -0300

    Subindo projeto loja games

---
+++ src/main/java/com/generation/loja_gamer/controller/ProdutosController.java
@@ -0,0 +1,93 @@
+package com.generation.loja_gamer.controller;
+
+
+
+import com.generation.loja_gamer.model.Produto;
+
+import com.generation.loja_gamer.repository.CategoriaRepository;
+import com.generation.loja_gamer.repository.ProdutosRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.math.BigDecimal;
+import java.util.List;
+
+@RestController
+@RequestMapping("/produto")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class ProdutosController {
+
+    @Autowired
+    private ProdutosRepository produtosRepository;
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+
+
+    @GetMapping
+    public ResponseEntity<List<Produto>> getAll() {
+        return (ResponseEntity.ok(produtosRepository.findAll()));
+    }
+    @GetMapping("{id}")
+    public ResponseEntity<Produto> getById(@PathVariable Long id) {
+        return produtosRepository.findById(id)
+                .map(resposta -> ResponseEntity.ok(resposta))
+                .orElse(ResponseEntity.notFound().build());
+    }
+    @GetMapping("/nome/{nome}")
+    public ResponseEntity<List<Produto>> getByNome(@PathVariable String nome) {
+        return ResponseEntity.ok( produtosRepository.findAllByNomeContainingIgnoreCase(nome));
+
+    }
+
+    @PostMapping
+    public ResponseEntity<Produto> postProduto(@Valid @RequestBody Produto produto){
+        return categoriaRepository.findById(produto.getCategoria().getId())
+                .map(resposta -> ResponseEntity.status(HttpStatus.CREATED).body(produtosRepository.save(produto)))
+                .orElse(ResponseEntity.badRequest().build());
+    }
+
+    @PutMapping
+    public ResponseEntity<Produto> putProduto(@Valid @RequestBody Produto produto) {
+
+        if (produtosRepository.existsById(produto.getId())){
+
+            return categoriaRepository.findById(produto.getCategoria().getId())
+                    .map(resposta -> ResponseEntity.status(HttpStatus.OK).body(produtosRepository.save(produto)))
+                    .orElse(ResponseEntity.badRequest().build());
+        }
+
+        return ResponseEntity.notFound().build();
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteProduto(@PathVariable Long id) {
+
+        return produtosRepository.findById(id)
+                .map(resposta -> {
+                    produtosRepository.deleteById(id);
+                    return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/preco_maior/{preco}")
+    public ResponseEntity<List<Produto>> getPrecoMaiorQue(@PathVariable BigDecimal preco){
+        return ResponseEntity.ok(produtosRepository.findByPrecoGreaterThanOrderByPreco(preco));
+    }
+
+
+
+    @GetMapping("/preco_menor/{preco}")
+    public ResponseEntity<List<Produto>> getPrecoMenorQue(@PathVariable BigDecimal preco){
+        return ResponseEntity.ok(produtosRepository.findByPrecoLessThanOrderByPrecoDesc(preco));
+    }
+
+}
+
+
+
---
+++ src/main/java/com/generation/loja_gamer/model/Produto.java
@@ -0,0 +1,111 @@
+package com.generation.loja_gamer.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "Nome é obrigatório!")
+    private String nome;
+
+
+
+    private int estoque;
+
+
+
+    @Column(name = "data_lancamento")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate dataLancamento;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    @NotNull(message = "Preço é obrigatório!")
+    @Positive(message = "O preço deve ser maior do que zero!")
+    private BigDecimal preco;
+
+    private String imagem;
+
+    @ManyToOne
+    @JsonIgnoreProperties("produto")
+    private Categoria categoria;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int estoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+
+
+
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public String getImagem() {
+        return this.imagem;
+    }
+
+    public void setImagem(String imagem) {
+        this.imagem = imagem;
+    }
+
+    public LocalDate getDataLancamento() {
+        return this.dataLancamento;
+    }
+
+    public void setDataLancamento(LocalDate dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+}
